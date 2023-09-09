26d7e49758da26ef3544c63d86d7e9d1fa101222
Maksim Hrynevich <maxim.grinevich@yahoo.com>
2023-09-09 15:45:24 +0200

    Release 1.0

--- src/main/resources/sql/Administration.sql
+++ src/main/resources/sql/Administration.sql
@@ -1,16 +1,15 @@
 CREATE USER 'jorvik-restaurant'@'localhost' IDENTIFIED BY 'jorvik-restaurant';
-GRANT SELECT, INSERT, UPDATE, DELETE ON jorvik.menu_type TO 'jorvik-restaurant'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON jorvik.dish TO 'jorvik-restaurant'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON jorvik.menu_item TO 'jorvik-restaurant'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON jorvik.Menu_Type TO 'jorvik-restaurant'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON jorvik.Dish TO 'jorvik-restaurant'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON jorvik.Menu_Item TO 'jorvik-restaurant'@'localhost';
 
 CREATE USER 'jorvik-housekeeping'@'localhost' IDENTIFIED BY 'jorvik-housekeeping';
-GRANT SELECT ON jorvik.room TO 'jorvik-housekeeping'@'localhost';
-GRANT SELECT ON jorvik.room_reservation TO 'jorvik-housekeeping'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON jorvik.cleaning_history TO 'jorvik-housekeeping'@'localhost';
+GRANT SELECT ON jorvik.Room TO 'jorvik-housekeeping'@'localhost';
+GRANT SELECT ON jorvik.Room_Reservation TO 'jorvik-housekeeping'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON jorvik.Cleaning_History TO 'jorvik-housekeeping'@'localhost';
 
 CREATE USER 'jorvik-admin'@'localhost' IDENTIFIED BY 'jorvik-admin';
 GRANT SELECT, INSERT, UPDATE, DELETE ON jorvik.* TO 'jorvik-admin'@'localhost';
 
 CREATE USER 'BackendAPI-Connection'@'localhost' IDENTIFIED BY 'BackendAPI-Connection';
 GRANT SELECT, INSERT, UPDATE, DELETE ON jorvik.* TO 'BackendAPI-Connection'@'localhost';
-
